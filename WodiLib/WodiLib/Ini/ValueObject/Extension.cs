c4a8852aa8c641fb58c977b45a098880477cbcb9
カメスケ <winterer_snowguard_2@yahoo.co.jp>
2019-10-06 18:40:55 +0900

    Ver 0.5.1.1
    - Extension クラスのインスタンスが null のとき、 string にキャストしようとすると例外が発生する不具合修正

--- WodiLib/WodiLib/Ini/ValueObject/Extension.cs
+++ WodiLib/WodiLib/Ini/ValueObject/Extension.cs
@@ -107,7 +107,7 @@ namespace WodiLib.Ini
         /// <returns>変換したインスタンス</returns>
         public static implicit operator string(Extension src)
         {
-            return src.Value;
+            return src?.Value;
         }
 
         // _/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
